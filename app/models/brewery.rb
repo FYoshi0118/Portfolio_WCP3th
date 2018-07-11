09ba0de8c763bad44deb39a6152f925880ed6105
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-11 20:24:31 +0000

    Breweryモデルのバリデーションとテスト追加

--- app/models/brewery.rb
+++ app/models/brewery.rb
@@ -1,5 +1,8 @@
 class Brewery < ApplicationRecord
   validates :name, presence: true
+  validates :name_kana, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
   # associarion
   has_many :sakes
 end
--- spec/models/brewery_spec.rb
+++ spec/models/brewery_spec.rb
@@ -2,37 +2,41 @@ require 'rails_helper'
 
 RSpec.describe Brewery, type: :model do
   describe '正常系のテスト' do
-    it 'is valid with a name, name_kana, post_code, address' do
-      brewery = Brewery.new(
-        name: '藤本雅一酒造醸',
-        name_kana: 'フジモトミヤビイチシュゾウジョウ',
-        post_code: '5830024',
-        address: '大阪府藤井寺市藤井寺2丁目1番10号'
-      )
-      expect(brewery).to be_valid
+    it 'has a valid factory' do
+      expect(FactoryBot.build(:brewery)).to be_valid
     end
-    
-    it 'is valid with all colums( name, name_kana, post_code, address, phone_number, email, url )' do
-      brewery = Brewery.new(
-        name: '藤本雅一酒造醸',
-        name_kana: 'フジモトミヤビイチシュゾウジョウ',
-        post_code: '5830024',
-        address: '大阪府藤井寺市藤井寺2丁目1番10号',
-        phone_number: '0729550018',
-        email: 'email@nazo.com',
-        url: 'http://www.fujimotosyuzou.com/index.html'
-      )
-      expect(brewery).to be_valid
+
+    it 'has a valid factory(add to phone_number, email, url)' do
+      expect(FactoryBot.build(:brewery, phone_number: '0729550018', email: 'email@nazo.com', url: 'http://www.fujimotosyuzou.com/index.html')).to be_valid
     end
   end
-  
+
   describe '異常系のテスト' do
     context 'when the value is empty' do
       it 'is invalid without a name' do
-        brewery = Brewery.new(name: nil)
+        brewery = FactoryBot.build(:brewery, name: nil)
         brewery.valid?
         expect(brewery.errors[:name]).to include("can't be blank")
       end
+
+      it 'is invalid without a name_kana' do
+        brewery = FactoryBot.build(:brewery, name_kana: nil)
+        brewery.valid?
+        expect(brewery.errors[:name_kana]).to include("can't be blank")
+      end
+
+      it 'is invalid without a post_code' do
+        brewery = FactoryBot.build(:brewery, post_code: nil)
+        brewery.valid?
+        expect(brewery.errors[:post_code]).to include("can't be blank")
+      end
+
+      it 'is invalid without a address' do
+        brewery = FactoryBot.build(:brewery, address: nil)
+        brewery.valid?
+        expect(brewery.errors[:address]).to include("can't be blank")
+      end
     end
   end
+
 end
