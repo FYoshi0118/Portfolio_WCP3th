b6f54c8e0bfe7300dd881fe66002ac16c3ead64f
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-11 09:48:15 +0000

    userモデルのバリデーションとテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,38 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name_code, presence: true, uniqueness: true
+
+  validates :nickname, presence: true
+
+  # validates :birthday,
+  #   format: {
+  #     with: /\A\d{4}\/0[1-9]\z|\A\d{4}\/1[0-2]\z/, # YYYY/01～YYYY/12まで
+  #     message: 'YYYY/MMの形式（半角数字）で入力して下さい。'
+  #   }
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true,
+    format: {
+      with: VALID_EMAIL_REGEX, 
+      message: '記入内容を確認して下さい。'
+    }
+
+  validates :password, presence: true,
+    length: {
+      in: 6..20,
+      message: '6～20文字以内で入力して下さい。'
+    }
+
+  validates :introduction,
+  length: {
+    maximum: 200,
+    message: '200文字以下で入力して下さい。'
+  }
+
+  validates :status, presence: true
+  
+  # association
+  has_many :posts
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,87 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '正常系のテスト'do
+    it 'name_code、nickname、email、passwordがあれば有効な状態であること' do
+      user = User.new(
+        name_code:  '@test',
+        nickname:   'テスト太郎',
+        email:      'test@wcp.com',
+        password:   'password',
+      )
+      expect(user).to be_valid
+    end
+    
+    it 'statusはデフォルトが0であること' do
+      user = User.new()
+      user.valid?
+      expect(user.status).to eq 0
+    end
+  end
+
+  describe '異常系のテスト' do
+    before do
+      User.create(
+        name_code:  '@tarou',
+        nickname:   'テスト太郎',
+        email:      'tarou@wcp.com',
+        password:   'password',
+        status:     0
+      )
+    end
+
+    context '値が空の場合' do
+      it 'name_codeがなければ無効な状態であること' do
+        user = User.new(name_code: nil)
+        user.valid?
+        expect(user.errors[:name_code]).to include("can't be blank")
+      end
+
+      it 'nicknameがなければ無効な状態であること' do
+        user = User.new(nickname: nil)
+        user.valid?
+        expect(user.errors[:nickname]).to include("can't be blank")
+      end
+
+      it 'emailがなければ無効な状態であること' do
+        user = User.new(email: nil)
+        user.valid?
+        expect(user.errors[:email]).to include("can't be blank")
+      end
+    end
+
+    context '値が重複している場合' do
+      it '重複したname_codeなら無効な状態であること' do
+        user = User.new(
+          name_code:  '@tarou',
+          nickname:   'テスト次郎',
+          email:      'jirou@wcp.com',
+          password:   'password',
+          status:     0
+        )
+        user.valid?
+        expect(user.errors[:name_code]).to include('has already been taken')
+      end
+
+      it '重複したemailなら無効な状態であること' do
+        user = User.new(
+          name_code:  '@jirou',
+          nickname:   'テスト次郎',
+          email:      'tarou@wcp.com',
+          password:   'password',
+          status:     0
+        )
+        user.valid?
+        expect(user.errors[:email]).to include('has already been taken')
+      end
+    end
+
+    context '入力制限の確認' do
+      it 'name_codeが21文字以上なら無効な状態であること'
+      it 'name_codeが半角英数字と-_以外なら無効な状態であること'
+      it 'passwordが5文字以下なら無効な状態であること'
+      it 'passwordが21文字以上なら無効な状態であること'
+      it 'introductionが201文字以上なら無効な状態であること'
+    end
+  end
 end
