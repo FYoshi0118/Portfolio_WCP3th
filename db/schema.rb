ae376103751d2bdcd578c9c2f84598675594f43f
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-11 11:34:09 +0000

    Postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180711064038) do
+ActiveRecord::Schema.define(version: 20180711104107) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -35,6 +35,19 @@ ActiveRecord::Schema.define(version: 20180711064038) do
     t.index ["is_deleted"], name: "index_admins_on_is_deleted"
     t.index ["is_main_administer"], name: "index_admins_on_is_main_administer"
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "sake_id", null: false
+    t.text "content", null: false
+    t.text "image_id"
+    t.integer "star", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["content"], name: "index_posts_on_content"
+    t.index ["sake_id"], name: "index_posts_on_sake_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
