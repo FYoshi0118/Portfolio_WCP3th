082610308fb0eb3066221293ed882d66c96e9c1e
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-11 04:14:27 +0000

    postsコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
+  get 'posts/show'
+
+  get 'posts/new'
+
+  get 'posts/create'
+
+  get 'posts/edit'
+
+  get 'posts/update'
+
+  get 'posts/destroy'
+
   get '/about' => 'static_pages#about'
   root :to => 'static_pages#top'
 end
