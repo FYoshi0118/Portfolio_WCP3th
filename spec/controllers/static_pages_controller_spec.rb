b521f4dd174088f1a8093a50a93b7dc51233c3c5
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-13 21:00:40 +0000

    static_pagesコントローラーのテスト追加

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -2,33 +2,29 @@ require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
 
-  describe '#top' do
+  describe "#top" do
     before do 
       get :top
     end
-    
-    it 'responds successfully' do
-      expect(response).to be_success
-    end
-    
-    it 'returns a 200 response' do
-      expect(response).to have_http_status '200'
-    end
-  end
 
-  describe '#about' do
-    before do
-      get :about
-    end
-    
     it "responds successfully" do
-      get :about
       expect(response).to be_success
     end
-    
-    it 'returns a 200 response' do
-      expect(response).to have_http_status '200'
+    it "returns a 200 response" do
+      expect(response).to have_http_status "200"
     end
   end
 
+  describe "#about" do
+    before do 
+      get :about
+    end
+
+    it "responds successfully" do
+      expect(response).to be_success
+    end
+    it "returns a 200 response" do
+      expect(response).to have_http_status "200"
+    end
+  end
 end
