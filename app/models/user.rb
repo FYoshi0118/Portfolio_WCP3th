7fd4e3f90d436d407bdfcbd8263b767c9d185b7a
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-14 09:27:41 +0000

    Userモデルのテスト追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,11 +12,12 @@ class User < ApplicationRecord
     
   validates :nickname, presence: true
 
-  # validates :birthday,
-  #   format: {
-  #     with: /\A\d{4}\/0[1-9]\z|\A\d{4}\/1[0-2]\z/, # YYYY/01～YYYY/12まで
-  #     message: "YYYY/MMの形式（半角数字）で入力して下さい。"
-  #   }
+  validates :birthday,
+    format: {
+      with: /\A[1-2]\d{3}\/0[1-9]\z|\A[1-2]\d{3}\/1[0-2]\z/, # YYYY/01～YYYY/12まで
+      allow_blank: true,
+      message: "YYYY/MMの形式（半角数字）で入力して下さい。"
+    }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true,
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,13 @@ FactoryBot.define do
     nickname 'テスト太郎'
     sequence(:email) { Faker::Internet.email }
     password 'password'
+
+    factory :birthday do
+      sequence(:birthday) { "1" + "9" + Faker::Number.between(50, 99).to_s + "/" + "0" + Faker::Number.between(1, 9).to_s}
+    end
+
+    factory :introduction do
+      sequence(:introduction) { Faker::Lorem.characters(10..100) }
+    end
   end
 end
