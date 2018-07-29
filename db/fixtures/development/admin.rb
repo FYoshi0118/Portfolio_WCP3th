5285795498c4431f36f537e864a2f7a716c2e4b3
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-07-29 18:37:49 +0000

    Adminテストデータ変更

--- db/fixtures/development/admin.rb
+++ db/fixtures/development/admin.rb
@@ -2,7 +2,7 @@ Admin.seed do |s|
   s.id = 1
   s.family_name = "管理者"
   s.given_name = "太郎"
-  s.email = "kanrisya@tarou.com"
+  s.email = "admin@tarou.com"
   s.password = "password"
   s.is_main_administer = "true"
   s.is_deleted = "false"
@@ -12,7 +12,7 @@ Admin.seed do |s|
   s.id = 2
   s.family_name = "管理者"
   s.given_name = "次郎"
-  s.email = "kanrisya@jirou.com"
+  s.email = "admin@jirou.com"
   s.password = "password"
   s.is_main_administer = "false"
   s.is_deleted = "false"
@@ -22,7 +22,7 @@ Admin.seed do |s|
   s.id = 3
   s.family_name = "管理者"
   s.given_name = "三郎"
-  s.email = "kanrisya@saburou.com"
+  s.email = "admin@saburou.com"
   s.password = "password"
   s.is_main_administer = "false"
   s.is_deleted = "true"
