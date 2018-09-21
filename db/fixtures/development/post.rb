6637f537995a46f1fc9ff18c08ac81f83a6d6aef
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-09-21 07:10:45 +0000

    postページ修正

--- db/fixtures/development/post.rb
+++ db/fixtures/development/post.rb
@@ -1,7 +1,134 @@
-Post.seed(:id,
-  { id: 1, user_id: 5, sake_id: 1, content: "テストcontent1", image_id: "", star: 2 },
-  { id: 2, user_id: 5, sake_id: 1, content: "テストcontent2", image_id: "", star: 3 },
-  { id: 3, user_id: 5, sake_id: 2, content: "テストcontent3", image_id: "", star: 4 },
-  { id: 4, user_id: 5, sake_id: 3, content: "テストcontent4", image_id: "", star: 2 },
-  { id: 5, user_id: 5, sake_id: 1, content: "テストcontent5", image_id: "", star: 5 }
-)
+Post.seed do |s|
+  s.id = 1
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 2
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 3
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 4
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 5
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 6
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 7
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 8
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 9
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 10
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 11
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 12
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 13
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 14
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
+
+Post.seed do |s|
+  s.id = 15
+  s.user_id = 5
+  s.sake_id = 
+  s.content = ""
+  s.image_id = ""
+  s.star = 
+end
