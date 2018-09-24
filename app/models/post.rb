9581bf55ada5a9f8e724857f8543913d3c945315
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-09-24 21:07:07 +0000

    いろいろ

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ class Post < ApplicationRecord
   belongs_to :sake, optional: true
   belongs_to :user, optional: true, counter_cache: true
 
-  validates :content, presence: true, length: {maximum: 200}
+  validates :content, length: {maximum: 200}
   validates :star, presence: true,
                    length: {
                      minimum: 0, maximum: 5,
--- db/migrate/20180711104107_create_posts.rb
+++ db/migrate/20180711104107_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.1]
     create_table :posts do |t|
       t.integer :user_id, null: false, index: true
       t.integer :sake_id, null: false, index: true
-      t.text    :content, null: false, index: true
+      t.text    :content
       t.text    :image_id
       t.integer :star,    null: false, default: 0
 
