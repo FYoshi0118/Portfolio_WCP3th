e900baf44c152e4ef952ac96777be046a08011dd
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-09-24 19:01:41 +0000

    kaminari

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,9 +5,6 @@ class PostsController < ApplicationController
   before_action :check_login
 
   def index
-    # @posts = Post.where(user_id: current_user.id)
-    # @user = User.find(current_user.id)
-    # @user = User.page(params[:page]).order(:id)
     @posts = Post.page(params[:page]).order(:created_at)
   end
 
