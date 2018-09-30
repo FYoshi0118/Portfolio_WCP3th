d48f648ffe7db633cb37eb18d9520a583ca3d44e
FYoshi0118 <yoshiki.f0118@gmail.com>
2018-09-30 16:42:31 +0000

    デバッグ用ユーザー追加

--- db/fixtures/development/user.rb
+++ db/fixtures/development/user.rb
@@ -4,5 +4,6 @@ User.seed(:id,
            introduction: "これはテストです。" },
   { id: 3, nickname: "テスト3", email: "tester3@wcp.com", password: "password", status: 1 },
   { id: 4, nickname: "テスト4", email: "tester4@wcp.com", password: "password", status: 2 },
-  { id: 5, nickname: "テスト太郎", email: "user@tarou.com", password: "password", status: 0 }
+  { id: 5, nickname: "テスト太郎", email: "user@tarou.com", password: "password", status: 0 },
+  { id: 6, nickname: "デバッグ太郎", email: "debug@tarou.com", password: "password", status: 0 }
 )
